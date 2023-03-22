a0aa975e631c9d4c89a4c29cf8c9695c20c2efd8
Syeda Ambreen Yawar <112860297+SyedaAmbreenYawars345523@users.noreply.github.com>
2023-03-22 12:47:45 +0100

    Create average_meter.py

---
+++ average_meter.py
@@ -0,0 +1,21 @@
+
+class AverageMeter(object):
+    """
+    Keeps track of most recent, average, sum, and count of a metric.
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val  # loss.item
+        self.sum += val * n
+        self.count += n # divider
+        self.avg = self.sum / self.count
+
